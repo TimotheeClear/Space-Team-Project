fb7deeb96de31849ef08aa88a663114635830bf1
Timothee <timothee.clear@gmail.com>
2022-01-11 16:55:02 +0100

    empty function to connect (mainPage.kt)

--- app/src/main/java/com/example/spacedim/MainActivity.kt
+++ app/src/main/java/com/example/spacedim/MainActivity.kt
@@ -3,6 +3,10 @@ package com.example.spacedim
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.spacedim.databinding.ActivityMainBinding
 import com.squareup.moshi.JsonClass
 import com.squareup.moshi.Moshi
 import okhttp3.*
@@ -13,50 +17,13 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
-
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         Log.i("MainPage","onCreate called")
-        run()
     }
 
-    private val client = OkHttpClient()
-/*    private val moshi = Moshi.Builder().build()
-    private val gistJsonAdapter = moshi.adapter(Gist::class.java)*/
-
-    fun run() {
-        val request = Request.Builder()
-            .url("http://spacedim.async-agency.com:8081/api/users")
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                e.printStackTrace()
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                response.use {
-                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
-                    val oui = response.body!!.string()
-                    try {
-                        val jsonObjects = JSONObject(oui)
-                        Log.i("JSON_ALL", jsonObjects.toString() )
-
-                    } catch (err: JSONException) {
-                        Log.d("Error", err.toString())
-                    }
-                }
-            }
-        })
-    }
-
-/*    @JsonClass(generateAdapter = true)
-    data class Gist(var files: Map<String, GistFile>?)
-
-    @JsonClass(generateAdapter = true)
-    data class GistFile(var content: String?)*/
-        
     override fun onStart() {
         super.onStart()
         Log.i("MainPage", "onStart called")
--- app/src/main/java/com/example/spacedim/MainPage.kt
+++ app/src/main/java/com/example/spacedim/MainPage.kt
@@ -1,30 +1,71 @@
 package com.example.spacedim
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.databinding.DataBindingUtil
 import com.example.spacedim.databinding.FragmentMainPageBinding
+import okhttp3.*
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
 
 class MainPage : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val binding = DataBindingUtil.inflate<FragmentMainPageBinding>(inflater,
-            R.layout.fragment_main_page,container,false)
+        val binding = DataBindingUtil.inflate<FragmentMainPageBinding>(inflater, R.layout.fragment_main_page,container,false)
 
        binding.joinButton.setOnClickListener{view : View ->
+           identifyUser()
+
            view.findNavController().navigate(R.id.action_mainPage_to_getReady)
        }
         setHasOptionsMenu(true)
 
         return binding.root
     }
+
+    private val client = OkHttpClient()
+
+    fun getUsers() {
+        val request = Request.Builder()
+            .url("http://spacedim.async-agency.com:8081/api/users")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+                    val oui = response.body!!.string()
+                    try {
+                        val jsonObjects = JSONObject(oui)
+                        Log.i("JSON_ALL", jsonObjects.toString() )
+
+                    } catch (err: JSONException) {
+                        Log.d("Error", err.toString())
+                    }
+                }
+            }
+        })
+    }
+
+    fun getPseudo() {}
+
+    fun identifyUser() {}
+
 
 }
