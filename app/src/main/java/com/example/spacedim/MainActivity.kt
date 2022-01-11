e19bedc049b6713cfc1886ab6bb7d908d71ec10a
Timothee <timothee.clear@gmail.com>
2022-01-11 15:52:57 +0100

    get user from api in MainActivity.kt

--- app/src/main/java/com/example/spacedim/MainActivity.kt
+++ app/src/main/java/com/example/spacedim/MainActivity.kt
@@ -3,6 +3,13 @@ package com.example.spacedim
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.squareup.moshi.JsonClass
+import com.squareup.moshi.Moshi
+import okhttp3.*
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +18,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i("MainPage","onCreate called")
+        run()
     }
+
+    private val client = OkHttpClient()
+/*    private val moshi = Moshi.Builder().build()
+    private val gistJsonAdapter = moshi.adapter(Gist::class.java)*/
+
+    fun run() {
+        val request = Request.Builder()
+            .url("http://spacedim.async-agency.com:8081/api/users")
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+                    val oui = response.body!!.string()
+                    try {
+                        val jsonObjects = JSONObject(oui)
+                        Log.i("JSON_ALL", jsonObjects.toString() )
+
+                    } catch (err: JSONException) {
+                        Log.d("Error", err.toString())
+                    }
+                }
+            }
+        })
+    }
+
+/*    @JsonClass(generateAdapter = true)
+    data class Gist(var files: Map<String, GistFile>?)
+
+    @JsonClass(generateAdapter = true)
+    data class GistFile(var content: String?)*/
         
     override fun onStart() {
         super.onStart()
