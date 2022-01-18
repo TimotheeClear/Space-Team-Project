d580147132e7be6d45ac5496d62e3e9ed7ca13d6
Keyto-Shouko <balvesca@gmail.com>
2022-01-18 11:50:42 +0100

    revert to old version

--- app/src/main/java/com/example/spacedim/network/User.kt
+++ app/src/main/java/com/example/spacedim/network/User.kt
@@ -1,16 +1,10 @@
 package com.example.spacedim.network
 
 import com.example.spacedim.State
-class User {
-    enum class State(val value: Int) {
-        WAITING(0), READY(1), IN_GAME(2), OVER(3)
-    }
-
-    data class User(
+ data class User(
         val id: Int,
         val name: String,
         val avatar: String,
         val score: Int,
         var state: State = State.OVER
     )
-}
