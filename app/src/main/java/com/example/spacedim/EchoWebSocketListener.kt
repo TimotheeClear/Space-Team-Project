b6f5708178a3ab7a6e7e5c33dbaf2e5a1bb0da3c
Timothee <timothee.clear@gmail.com>
2022-01-18 15:11:44 +0100

    correction ws start()

--- app/src/main/java/com/example/spacedim/EchoWebSocketListener.kt
+++ app/src/main/java/com/example/spacedim/EchoWebSocketListener.kt
@@ -9,21 +9,21 @@ class EchoWebSocketListener : WebSocketListener() {
     private val NORMAL_CLOSURE_STATUS: Int = 1000;
 
     override fun onOpen(webSocket: WebSocket, response: okhttp3.Response) {
-        Log.i("truc", "open")
+        Log.i("WebSocket", "open")
     }
 
     override fun onMessage(webSocket: WebSocket, text: String) {
         super.onMessage(webSocket, "Receiving : " + text)
-        Log.i("truc", "Receiving : " + text)
+        Log.i("WebSocket", "Receiving : " + text)
     }
 
     override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
         super.onMessage(webSocket, "Receiving bytes : " + bytes)
-        Log.i("truc", "Receiving bytes : " + bytes)
+        Log.i("WebSocket", "Receiving bytes : " + bytes)
     }
 
     override fun onFailure(webSocket: WebSocket, t: Throwable, response: okhttp3.Response?) {
         super.onFailure(webSocket, t, response)
-        Log.i("truc","Error")
+        Log.i("WebSocket","Error")
     }
 }
