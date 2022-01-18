11d5728e0941693d2a1479dd459c2944e901223e
Keyto-Shouko <balvesca@gmail.com>
2022-01-18 11:29:08 +0100

    modified User so it correspond to model class

--- app/src/main/java/com/example/spacedim/network/User.kt
+++ app/src/main/java/com/example/spacedim/network/User.kt
@@ -1,5 +1,16 @@
 package com.example.spacedim.network
 
 import com.example.spacedim.State
+class User {
+    enum class State(val value: Int) {
+        WAITING(0), READY(1), IN_GAME(2), OVER(3)
+    }
 
-data class User(val id : Int, val name: String, val avatar : String, val score : Int, var state: State = State.OVER)
+    data class User(
+        val id: Int,
+        val name: String,
+        val avatar: String,
+        val score: Int,
+        var state: State = State.OVER
+    )
+}
